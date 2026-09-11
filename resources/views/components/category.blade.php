3a284218d11df7b4f40ade9e0265e18c1c8a8344
holeeho <holeeho@gmail.com>
2026-09-11 09:37:38 +0700

    reboot

--- resources/views/components/category.blade.php
+++
@@ -1,4 +0,0 @@
-<a href="{{ route('shop', ['category' => $cate->id]) }}" class="category" style="background-image: url('{{ asset('storage/'.$cate->image) }}')">
-    <div class="screen"></div>
-    <h3>{{ ucfirst($cate->title) }}</h3>
-</a>
--- resources/views/components/product.blade.php
+++
@@ -1,7 +0,0 @@
-<a href="{{ route('product', ['product' => $product->id]) }}" class="product_card">
-    <div>
-        <img src="{{ asset('storage/'.$product->image) }}" alt="{{ ucfirst($product->title) }}">
-    </div>
-    <h3>{{ ucfirst($product->title) }}</h3>
-    <p>{{ "$".$product->price }}</p>
-</a>
